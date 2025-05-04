759afffe50f0f4a71a3f4dee84dea19a724b9893
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2025-05-05 02:16:31 +0300

    Merge

--- frontend/src/pages/AdminOrders.jsx
+++ frontend/src/pages/AdminOrders.jsx
@@ -1,3 +1,52 @@
-export default function AdminOrdes() {
-  return <section className="admin-orders-wrapper">asd</section>;
+import {useEffect, useState} from 'react';
+import {useOrder} from '../hooks/useOrder';
+import AdminOrdersRow from '../components/AdminOrdersRow';
+import '../css/admin-orders.css';
+
+export default function AdminOrders() {
+  const {getActiveOrders} = useOrder();
+  const [ordersData, setOrdersData] = useState([]);
+
+  const loadOrdersData = async () => {
+    const ordersData = await getActiveOrders();
+
+    const sortedOrders = [...ordersData.data].sort(
+      (a, b) => new Date(a.created_at) - new Date(b.created_at)
+    );
+    setOrdersData(sortedOrders);
+    console.log(sortedOrders);
+  };
+  useEffect(() => {
+    loadOrdersData();
+  }, []);
+
+  return (
+    <section className="admin-orders-wrapper">
+      <h1>Aktiiviset tilaukset</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Asiakkaan tiedot</th>
+            <th>Tuotteet</th>
+            <th>Tila</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ordersData && ordersData.length > 0 ? (
+            ordersData.map((order) => (
+              <AdminOrdersRow
+                key={order.id}
+                order={order}
+                refreshOrders={loadOrdersData}
+              />
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3">Ei aktiivisia tilauksia</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </section>
+  );
 }
