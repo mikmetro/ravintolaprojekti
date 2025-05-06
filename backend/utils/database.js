a591c1be8465c38d10ffd6ff1c5042b7065e7b50
mikmetro <mikkomav@metropolia.fi>
2025-05-06 21:06:24 +0300

    Optional DB initialization

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -4,7 +4,7 @@ import fs from "fs";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -16,16 +16,19 @@ const pool = mysql.createPool({
 
 const promisePool = pool.promise();
 
-promisePool
-  .query(fs.readFileSync("./utils/init_db.sql", "utf8"))
-  .then(() => console.info("Database connected & initialized successfully."))
-  .catch((err) => {
-    if (err.code === "ER_TABLE_EXISTS_ERROR") {
-      // shit ass design, it shouldn't be an error really.
-      console.info("Database connected. (Tables already exist)");
-    } else {
-      console.error("error while creating the db:", err.message);
-    }
-  });
-console.info("Database connected & initialized.");
+if (process.env.DB_INIT === "true") {
+  promisePool
+    .query(fs.readFileSync("./utils/init_db.sql", "utf8"))
+    .then(() => console.info("Database connected & initialized successfully."))
+    .catch((err) => {
+      if (err.code === "ER_TABLE_EXISTS_ERROR") {
+        // shit ass design, it shouldn't be an error really.
+        console.info("Database connected. (Tables already exist)");
+      } else {
+        console.error("error while creating the db:", err.message);
+      }
+    });
+  console.info("Database connected & initialized.");
+}
+
 export default promisePool;
